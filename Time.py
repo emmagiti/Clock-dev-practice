7e714977a6367a1e2a56065492aa751088c42a7f
emmagiti <79483248+emmagiti@users.noreply.github.com>
2021-02-24 06:54:15 -0800

    First python code uploaded

---
+++ Time.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000,time)
+    
+label = Label(root, font=("ds-digital", 80), background = "black", foreground = "cyan")
+label.pack(anchor='center')
+time()
+
+mainloop()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
